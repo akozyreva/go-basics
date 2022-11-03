c1383fd1098eae28dfa9421c7663d2a472a6fa14
Anna Kozyreva <kozyreva.hanna@gmail.com>
2022-11-03 15:47:50 +0100

    add string example

---
+++ README.md
@@ -0,0 +1,7 @@
+# go-basics
+
+How to run specific file:
+
+```
+go run <path-to-go-file>
+```
---
+++ introduction/string_example.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Go"[0])         // it shows ASCII bytes code for char
+	fmt.Println(string("Hello Go"[0])) // it shows first letter
+}
