bb0c97fc8dfa44fc452057dd59c79f8aa3b7d6fa
Anna Kozyreva <kozyreva.hanna@gmail.com>
2022-11-03 16:51:26 +0100

    add multiple basic examples

--- introduction/string_example.go
+++ introduction/string_example.go
@@ -5,4 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println("Hello Go"[0])         // it shows ASCII bytes code for char
 	fmt.Println(string("Hello Go"[0])) // it shows first letter
+
+	// for symbols there will be ''
+	fmt.Println('a')  // and it shows bytes code, because Go saves symbols so
+	fmt.Println('\n') // the same - shows byte of symbol
+	fmt.Println("a")  // and it's row and shows a
 }
